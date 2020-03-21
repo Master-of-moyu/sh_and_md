60d3d7462e43f8ab2f27af3ce7e24d0d5002cf88
xiangqian <648261121@qq.com>
2020-03-21 19:24:39 +0800

    Nothing.

--- plot.py
+++ plot.py
@@ -1,34 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-X = np.linspace(-np.pi,np.pi,256,endpoint=True)
+plt.figure(1)
+X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
 C,S = np.cos(X), np.sin(X)
 
-plt.plot(X,C,'b--',label='cos')
-plt.plot(X,S,'r',label='sin')
+plt.plot(X, C, 'b--',label='cos')
+plt.plot(X, S, 'r',label='sin')
 
 plt.title("plot test")
 plt.legend(loc='upper right')
-
+# set axis range
+plt.xlim(-5, 5)
+plt.ylim(-1.2, 1.2)
+plt.grid(True)
 plt.show()
 
 # import numpy as np
 # import matplotlib.pyplot as plt
 
-# # Make some fake data.
-# a = b = np.arange(0, 3, .02)
-# c = np.exp(a)
-# d = c[::-1]
-
-# # Create plots with pre-defined labels.
-# fig, ax = plt.subplots()
-# ax.plot(a, c, 'k--', label='Model length')
-# ax.plot(a, d, 'k:', label='Data length')
-# ax.plot(a, c + d, 'k', label='Total message length')
-
-# legend = ax.legend(loc='upper center', shadow=True, fontsize='x-large')
-
-# # Put a nicer background color on the legend.
-# legend.get_frame().set_facecolor('C0')
-
+# a = np.arange(0, 3, 0.02)
+# b = np.square(a)
+# l = len(a)
+# max_x = a[l - 1]
+# max_y = b[l - 1]
+# plt.plot(a, b, label='x^2')
+# plt.title("plot test")
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.xlim(-1, max_x + 1)
+# plt.ylim(-1, max_y + 1)
+# plt.grid(True)
 # plt.show()
